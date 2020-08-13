5176d2f9b1665ecf6ef817358b67a88eac552e50
Dirk Stenger <stenger@lat-lon.de>
2020-08-13 19:37:27 +0200

    Formatting (#16)

--- src/main/java/org/opengis/cite/swecommon20/level1/CoreConcept.java
+++ src/main/java/org/opengis/cite/swecommon20/level1/CoreConcept.java
@@ -13,26 +13,24 @@ import org.opengis.cite.swecommon20.ETSAssert;
 import org.testng.annotations.Test;
 import org.xml.sax.SAXException;
 
-public class CoreConcept extends DataFixture{
+public class CoreConcept extends DataFixture {
 
-	@Test(groups = "CoreConceptsBase", description = "A.1.1 Core concepts are the base of all derived models. Validate the XML document using the XML schema document")
-	public void Core(){
-		URL entityCatalog = this.getClass().getResource("/org/opengis/cite/swecommon20/xsd/opengis/swe_2.0.1_flatten/swe_2.0.1.xsd");
-		Source source = new DOMSource(this.testSubject);
-		Validator validator;
-		try {
-			validator = CreateValidator(entityCatalog);
-			ETSAssert.assertSchemaValid(validator, source);
-		} catch (XMLStreamException | SAXException | IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	@Test(groups = "CoreConcepts", dependsOnGroups  = { "CoreConceptsBase" }, description="A.1 Validate all conformance classes in core concepts by using schematron file.")
-	public void checkCoreConceptsSchematron() {
-		URL schRef = this.getClass().getResource(
-				"/org/opengis/cite/swecommon20/sch/A.1.CoreConcepts.sch");
-		ETSAssert
-				.assertSchematronValid(schRef, new StreamSource(this.dataFile));
-	}
+    @Test(groups = "CoreConceptsBase", description = "A.1.1 Core concepts are the base of all derived models. Validate the XML document using the XML schema document")
+    public void Core() {
+        URL entityCatalog = this.getClass().getResource( "/org/opengis/cite/swecommon20/xsd/opengis/swe_2.0.1_flatten/swe_2.0.1.xsd" );
+        Source source = new DOMSource( this.testSubject );
+        Validator validator;
+        try {
+            validator = CreateValidator( entityCatalog );
+            ETSAssert.assertSchemaValid( validator, source );
+        } catch ( XMLStreamException | SAXException | IOException e ) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test(groups = "CoreConcepts", dependsOnGroups = { "CoreConceptsBase" }, description = "A.1 Validate all conformance classes in core concepts by using schematron file.")
+    public void checkCoreConceptsSchematron() {
+        URL schRef = this.getClass().getResource( "/org/opengis/cite/swecommon20/sch/A.1.CoreConcepts.sch" );
+        ETSAssert.assertSchematronValid( schRef, new StreamSource( this.dataFile ) );
+    }
 }
