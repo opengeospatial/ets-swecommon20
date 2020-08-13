4dfba36a6d5fa96968e65467011bf7a75b64ad3c
Dirk Stenger <stenger@lat-lon.de>
2020-08-13 19:45:58 +0200

    Add schema validation against SWECommon schema (#16)

--- src/main/java/org/opengis/cite/swecommon20/level1/CoreConcept.java
+++ src/main/java/org/opengis/cite/swecommon20/level1/CoreConcept.java
@@ -3,10 +3,13 @@ package org.opengis.cite.swecommon20.level1;
 import java.io.IOException;
 import java.net.URL;
 
+import javax.xml.XMLConstants;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.transform.Source;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 
 import org.opengis.cite.swecommon20.ETSAssert;
@@ -17,13 +20,14 @@ public class CoreConcept extends DataFixture {
 
     @Test(groups = "CoreConceptsBase", description = "A.1.1 Core concepts are the base of all derived models. Validate the XML document using the XML schema document")
     public void Core() {
-        URL entityCatalog = this.getClass().getResource( "/org/opengis/cite/swecommon20/xsd/opengis/swe_2.0.1_flatten/swe_2.0.1.xsd" );
+        URL schemaUrl = this.getClass().getResource( "/org/opengis/cite/swecommon20/xsd/opengis/swe_2.0.1_flatten/swe_2.0.1.xsd" );
         Source source = new DOMSource( this.testSubject );
         Validator validator;
         try {
-            validator = CreateValidator( entityCatalog );
+            Schema schema = SchemaFactory.newInstance( XMLConstants.W3C_XML_SCHEMA_NS_URI ).newSchema( schemaUrl );
+            validator = schema.newValidator();
             ETSAssert.assertSchemaValid( validator, source );
-        } catch ( XMLStreamException | SAXException | IOException e ) {
+        } catch ( SAXException e ) {
             e.printStackTrace();
         }
     }
